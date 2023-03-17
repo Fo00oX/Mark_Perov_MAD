ba052e50f5e37b381c2f6eb63fe5178c6b9cdd83
Markant <mark.perov@stud.fh-campuswien.ac.at>
2023-03-17 22:13:17 +0100

    Refactor

--- app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
+++ app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
@@ -28,44 +28,9 @@ fun DetailScreen(navController: NavController, movie: Movie) {
         topBar = { DetailTopBar(navController, movie.title) }
     ) {
         Column {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp),
-                shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-                elevation = 5.dp
-            ) {
-                Column {
-                    Box(
-                        modifier = Modifier
-                            .aspectRatio(16f / 9f)
-                            .fillMaxWidth()
-                    ) {
-                        Image(
-                            painter = rememberImagePainter(movie.images[0]),
-                            contentDescription = movie.title,
-                            contentScale = ContentScale.Crop
-                        )
-
-                        Box(
-                            modifier =
-                            Modifier.fillMaxSize().padding(10.dp),
-                            contentAlignment =
-                            Alignment.TopEnd
-                        ) {
-                            Icon(
-                                tint =
-                                MaterialTheme.colors.secondary,
-                                imageVector =
-                                Icons.Default.FavoriteBorder,
-                                contentDescription =
-                                "Add to favorites"
-                            )
-                        }
-                    }
-                }
-            }
+            MovieRow(movie = movie)
             ImageList(movie.images)
+
         }
     }
 }
