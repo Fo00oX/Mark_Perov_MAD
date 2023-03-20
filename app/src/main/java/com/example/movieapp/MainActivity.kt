99a60087779a33993bd26fce4c31f788476ad98e
Markant <mark.perov@stud.fh-campuswien.ac.at>
2023-03-20 21:47:49 +0100

    Rename Theme

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.movieapp.navigation.Navigation
-import com.example.movieapp.ui.theme.MyMovieTheme
+import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyMovieTheme {
+            MovieAppTheme {
                 Navigation()
             }
         }
--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MyMovieTheme(
+fun MovieAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
