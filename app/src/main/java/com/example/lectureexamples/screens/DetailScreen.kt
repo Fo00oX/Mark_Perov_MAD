a653e6cf46a1d71cf08d1ee6c5c10a4456514e97
Markant <mark.perov@stud.fh-campuswien.ac.at>
2023-03-20 18:32:46 +0100

    Add Padding

--- app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
+++ app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
@@ -4,19 +4,18 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import com.example.lectureexamples.components.MovieCard
 import com.example.lectureexamples.models.Movie
@@ -34,9 +33,12 @@ fun DetailScreen(navController: NavController, movie: Movie) {
         }
     }
 }
+
 @Composable
 fun ImageList(images: List<String>) {
-    LazyRow {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 8.dp)
+    ) {
         items(images) { image ->
             Card(
                 modifier = Modifier.padding(8.dp),
@@ -44,7 +46,7 @@ fun ImageList(images: List<String>) {
                 elevation = 4.dp
             ) {
                 Image(
-                    painter = rememberImagePainter(image),
+                    painter = rememberAsyncImagePainter(image),
                     contentDescription = null,
                     modifier = Modifier.size(250.dp),
                     contentScale = ContentScale.Crop
